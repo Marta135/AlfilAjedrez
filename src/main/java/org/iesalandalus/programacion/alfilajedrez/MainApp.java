9d0ff2d52d2990cf85f5b91735774d1858518963
marti <marti@192.168.1.34>
2020-11-30 13:42:15 +0100

    18.1 Creación del método mostrarAlfil de la clase MainApp.

---
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -0,0 +1,19 @@
+package org.iesalandalus.programacion.alfilajedrez;
+
+public class MainApp {
+
+	/*******ATRIBUTOS******/
+	private static Alfil alfil;
+	
+	
+	/**********MÉTODOS********/
+	
+	/**
+	 * Método mostrarAlfil
+	 * Método que imprime por consola la representación del Alfil
+	 */
+	private static void mostrarAlfil() {
+		
+		System.out.println(alfil);
+	}
+}
