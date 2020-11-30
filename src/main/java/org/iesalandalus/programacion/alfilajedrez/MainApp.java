59367554d6231306b9d43a3d1c94bf8385ac6bd1
marti <marti@192.168.1.34>
2020-11-30 13:46:52 +0100

    18.2 Creación del método mostrarMenu, de la clase MainApp.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -16,4 +16,26 @@ public class MainApp {
 		
 		System.out.println(alfil);
 	}
+	
+	/**
+	 * Método mostrarMenu
+	 * Método que imprime por consola el menú con las distintas opciones
+	 */
+	private static void mostrarMenu() {
+		
+		System.out.println("");
+		System.out.println("----------------------------------------------------------------------");
+		System.out.println("PROGRAMA QUE CONTROLA EL MOVIMIENTO DEL ALFIL EN EL TABLERO DE AJEDREZ");
+		System.out.println("----------------------------------------------------------------------");
+		System.out.println("");
+		System.out.println("1.- Crear un alfil por defecto. ");
+		System.out.println("2.- Crear un alfil de un color. ");
+		System.out.println("3.- Crear un alfil de un color en una columna inicial dada. ");
+		System.out.println("4.- Mover el alfil. ");
+		System.out.println("");
+		System.out.println("0.- Salir. ");
+		System.out.println("");
+		
+	}
+	
 }
