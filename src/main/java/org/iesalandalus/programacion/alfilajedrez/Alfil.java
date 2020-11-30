d1b6f57e76d05a27879c47ca10371ae98ac22c53
marti <marti@192.168.1.34>
2020-11-30 11:39:37 +0100

    11. Creación de los métodos get y set de la clase Alfil.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -6,4 +6,32 @@ public class Alfil {
 	private Color color;
 	private Posicion posicion;
 	
+	
+	/*********GETTERS Y SETTERS**********/
+	
+	private void setPosicion(Posicion posicion) {
+		
+		if (posicion==null)
+			throw new NullPointerException ("ERROR: La posición pasada como parámetro es null.");
+		else
+			this.posicion = new Posicion(posicion);
+	}
+	
+	public Posicion getPosicion() {
+		return new Posicion(posicion);
+	}
+	
+	private void setColor(Color color) {
+		
+		if (color==null)
+			throw new NullPointerException ("ERROR: El color pasado como parámetro es null.");
+		else
+			this.color = color;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
 }
+
