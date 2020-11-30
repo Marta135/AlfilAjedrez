288f38053c8e991bfcc56da2a2b20a919140a833
marti <marti@192.168.1.34>
2020-11-30 10:57:15 +0100

    5. Creación de los métodos get y set de la clase Posicion.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -6,4 +6,31 @@ public class Posicion {
 	private int fila;
 	private char columna;
 	
+	
+	/*********GETTERS Y SETTERS**********/
+	private void setFila(int fila) {
+		
+		if (fila<1 || fila>8)
+			throw new IllegalArgumentException ("ERROR: Fila no válida.");
+		
+		this.fila = fila;
+	}
+	
+	public int getFila() {
+		return fila;
+	}
+	
+	private void setColumna(char columna) {
+		
+		if (columna<'a' || columna>'h') 
+			throw new IllegalArgumentException ("ERROR: Columna no válida.");
+
+		this.columna = columna;
+	}
+	
+	public char getColumna() {
+		return columna;
+	}
+
+	
 }
